f42a6f669716639a4ea540356affc57261c09369
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2023-06-04 06:57:58 +0600

    Basic Setup with Mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+// import usersService from './app/modules/users/users.service'
+import usersRouter from './app/modules/users/users.route'
 
 const app: Application = express()
 app.use(cors())
@@ -7,7 +9,15 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+app.use('/api/v1/users/', usersRouter)
+
+// Testing
+app.get('/', async(req: Request, res: Response) => {
+  // await usersService.createUser({
+  //   id:'999',
+  //   password:'password',
+  //   role:'admin',
+  // })
   res.send('Working Successfully!')
 })
 
